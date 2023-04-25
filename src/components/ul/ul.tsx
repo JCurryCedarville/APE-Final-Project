910cd8fc73089e06750225a32996218961df7adb
Vix <eatahorsequickly@gmail.com>
2023-04-24 22:14:50 -0400

    test

---
+++ src/components/ul/ul.tsx
@@ -0,0 +1,14 @@
+import styles from './ul.module.scss';
+import classNames from 'classnames';
+
+export interface ULProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-uls-and-templates
+ */
+export const UL = ({ className }: ULProps) => {
+    return <div className={classNames(styles.root, className)}>UL</div>;
+};
