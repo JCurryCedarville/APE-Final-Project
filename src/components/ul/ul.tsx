07ed3744c9d0c3b89e97b9e5f5a13c734747e392
Vix <eatahorsequickly@gmail.com>
2023-04-29 14:22:11 -0400

    new accordian

--- src/components/ul/ul.tsx
+++ src/components/ul/ul.tsx
@@ -1,5 +1,6 @@
 import styles from './ul.module.scss';
 import classNames from 'classnames';
+import Accordion from '../Accordion';
 
 export interface ULProps {
     className?: string;
@@ -9,10 +10,66 @@ export interface ULProps {
  * This component was created using Codux's Default new component template.
  * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-uls-and-templates
  */
+const accordionItems = [
+    {
+      title: 'Accordion Item #1',
+      content: (
+        <div>
+          <strong>This is the first item's accordion body.</strong> It is hidden
+          by default, but shown when the title is clicked. It will also be
+          hidden if the title is clicked again or when another item is clicked.
+          You can pass HTML tags in the content such as <u>underline tag</u>,{' '}
+          <i>italic</i>, or even another list like this:
+          <ul>
+            <li>Bread</li>
+            <li>Eggs</li>
+            <li>Milk</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: 'Accordion Item #2',
+      content: (
+        <div>
+          <strong>This is the second item's accordion body.</strong> It is
+          hidden by default, but shown when the title is clicked. It will also
+          be hidden if the title is clicked again or when another item is
+          clicked. You can pass HTML tags in the content such as{' '}
+          <u>underline tag</u>, <i>italic</i>, or even another list like this:
+          <ul>
+            <li>Bread</li>
+            <li>Eggs</li>
+            <li>Milk</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: 'Accordion Item #3',
+      content: (
+        <div>
+          <strong>This is the third item's accordion body.</strong> It is hidden
+          by default, but shown when the title is clicked. It will also be
+          hidden if the title is clicked again or when another item is clicked.
+          You can pass HTML tags in the content such as <u>underline tag</u>,{' '}
+          <i>italic</i>, or even another list like this:
+          <ul>
+            <li>Bread</li>
+            <li>Eggs</li>
+            <li>Milk</li>
+          </ul>
+        </div>
+      ),
+    }
+];
+
 export const UL = ({ className }: ULProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <div>Imagine an accordian here</div>
+            <div className="container">
+            <Accordion items={accordionItems} />
+            </div>
         </div>
     );
 };
