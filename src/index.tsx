c242e257bb8294bac8605ef8f1787462d5e4fc52
Vix <eatahorsequickly@gmail.com>
2023-04-29 22:00:57 -0400

    breaking bad

--- src/index.tsx
+++ src/index.tsx
@@ -8,20 +8,20 @@ import path from "path";
 //import cors from "cors";
 
 
-const app: Express = express();
+//const app: Express = express();
 
-app.use(express.json());
+//app.use(express.json());
 //app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-res.send('Hello World From the Typescript Server!')
-});
+// app.get('/', (req: Request, res: Response) => {
+// res.send('Hello World From the Typescript Server!')
+// });
 
-const port = process.env.PORT || 8000;
+//const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
-});
+// app.listen(port, () => {
+// console.log(`Example app listening on port ${port}`)
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
