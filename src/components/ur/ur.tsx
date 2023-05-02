0335dd7eb511c759ffd7efa4b64265f864d82078
Christopher LaFave <clafave@cedarville.edu>
2023-05-02 17:08:02 -0400

    added react tables

--- src/components/ur/ur.tsx
+++ src/components/ur/ur.tsx
@@ -18,7 +18,7 @@ export const UR = ({ className }: URProps) => {
     function onUpdate(e: React.SyntheticEvent<HTMLFormElement>) {
         e.preventDefault();
     }
-    const [Data, setData] = useState({object: [{name: "string", password: "string", type: "string", planId: "string" }]});
+    const [Data, setData] = useState({object: [{course_id: "string", name: "string", term: "string", year: "string", credits: "string" }]});
 
     // useEffect(() => {
     //     if (document.getElementById('planId')?.getAttribute('value') !== "0") {  
@@ -34,31 +34,71 @@ export const UR = ({ className }: URProps) => {
             .then(data => setData(data))  
             .catch(err => console.log(err));
     };
+
+    var years:string[] = new Array();
+    Data.object.forEach(function(course){
+        if(!years.includes(course.year)){
+            years.push(course.year);
+        }
+    });
+    years.sort();
+
     return (
         <div className={classNames(styles.root, className)} onFocus={updateData}>
             <div className={styles.GridSchedule}>
-                {Data.object.map((val, key) => {
+                <div className={styles.Semester}>
+                    <h1>Transfered Classes</h1>
+                </div>
+                {years.map((year) => {
+                    if(year === years[0]){
+                        return(
+                            <div className={styles.Semester}>
+                                <h1>Fall {year}</h1>
+                            </div>
+                        )
+                    }
+                    else if(year === years[years.length-1]){
+                        return(
+                            <div className={styles.Semester}>
+                                <h1>Spring {year}</h1>
+                            </div>
+                        )
+                    }
+                    else{
+                        return(
+                            <>
+                                <div className={styles.Semester}>
+                                    <h1>Spring {year}</h1>
+                                </div>
+                                <div className={styles.Semester}>
+                                    <h1>Summer {year}</h1>
+                                </div>
+                                <div className={styles.Semester}>
+                                    <h1>Fall {year}</h1>
+                                </div>
+                            </>
+                        );
+                    }
+                    
+                })}
+
+                {/* {Data.object.map((val, key) => {
                     return (
                         <div className={styles.Semester} key={key} draggable>
+                            <td>{val.course_id}</td>
                             <td>{val.name}</td>
-                            <td>{val.password}</td>
-                            <td>{val.type}</td>
-                            <td>
-                                <form onSubmit={onUpdate} method='GET'>
-                                    <input type="hidden" name ="plan" value={val.planId} />
-                                    <button type='submit'>Click Me!</button>
-                                </form>
-                            </td>
+                            <td>{val.term}</td>
+                            <td>{val.year}</td>
+                            <td>{val.credits}</td>
                         </div>
                     );
-                })}
-                    
+                })} */}
+            </div>
                 <div className={styles.Notes}>
                 <label>
                     Notes: <textarea name="notes" id='notes' onBlur={SaveNotes}></textarea>
                 </label>
                 </div>
-            </div>
         </div>
     );
 };
